7035e92a45ef324d3b871ef0b90141c057f59fea
Acon-di <2413620125@qq.com>
2018-08-27 16:44:28 +0800

    no message

--- web/temp/compiled/position_get_adv_small.lbi.php
+++ web/temp/compiled/position_get_adv_small.lbi.php
@@ -1,7 +1,7 @@
 <div class="marBanner">
-    <?php $_from = $this->_var['ad_child']; if (!is_array($_from) && !is_object($_from)) { settype($_from, 'array'); }; $this->push_vars('', 'child_0_45800700_1535356797');if (count($_from)):
-    foreach ($_from AS $this->_var['child_0_45800700_1535356797']):
+    <?php $_from = $this->_var['ad_child']; if (!is_array($_from) && !is_object($_from)) { settype($_from, 'array'); }; $this->push_vars('', 'child_0_78417900_1535357891');if (count($_from)):
+    foreach ($_from AS $this->_var['child_0_78417900_1535357891']):
 ?>
-        <a href="<?php echo $this->_var['child_0_45800700_1535356797']['ad_link']; ?>" target="_blank"><img src="<?php echo $this->_var['child_0_45800700_1535356797']['ad_code']; ?>" width="<?php echo $this->_var['child_0_45800700_1535356797']['ad_width']; ?>" height="<?php echo $this->_var['child_0_45800700_1535356797']['ad_height']; ?>" alt="" /></a>
+        <a href="<?php echo $this->_var['child_0_78417900_1535357891']['ad_link']; ?>" target="_blank"><img src="<?php echo $this->_var['child_0_78417900_1535357891']['ad_code']; ?>" width="<?php echo $this->_var['child_0_78417900_1535357891']['ad_width']; ?>" height="<?php echo $this->_var['child_0_78417900_1535357891']['ad_height']; ?>" alt="" /></a>
     <?php endforeach; endif; unset($_from); ?><?php $this->pop_vars();; ?>                             	
 </div>
